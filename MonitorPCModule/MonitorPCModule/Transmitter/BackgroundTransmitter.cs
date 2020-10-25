fbdaccce0be05e5830ffc3332d18d2e62e5bb92c
Leslie M <leslie.marxen98@gmail.com>
2020-10-25 12:18:55 +0100

    Cleaned up BackgroundTransmitter.cs

--- MonitorPCModule/MonitorPCModule/Transmitter/BackgroundTransmitter.cs
+++ MonitorPCModule/MonitorPCModule/Transmitter/BackgroundTransmitter.cs
@@ -1,39 +1,24 @@
-﻿using OpenHardwareMonitor.Hardware;
-using System;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Timers;
+﻿using System.IO;
 
 namespace MonitorPCModule.Transmitter
 {
-    public class BackgroundTransmitter
+    public class BackgroundTransmitter : TcpTransmitter
     {
-        private string Hostname;
-        private int Port;
-        private string JpgPath;
+        private readonly string _jpgPath;
 
-        public BackgroundTransmitter(string hostname, int port, string jpg_path)
+        public BackgroundTransmitter(string hostname, int port, string jpgPath) : base(hostname, port)
         {
-            Hostname = hostname;
-            Port = port;
-            JpgPath = jpg_path;
+            _jpgPath = jpgPath;
         }
 
-        public void Start()
+        protected override PackageType GetPackageType()
         {
-            byte[] read_file = File.ReadAllBytes(JpgPath);
-            byte[] send_buffer = new byte[read_file.Length + 1];
-            send_buffer[0] = 0x01; // 0x01 = static image
-            Array.Copy(read_file, 0, send_buffer, 1, read_file.Length);
-            TcpClient client = new TcpClient(Hostname, Port);
-            NetworkStream stream = client.GetStream();
-            stream.Write(send_buffer, 0, send_buffer.Length);
-            stream.Close();
-            client.Close();
+            return PackageType.StaticBackgroundImage;
         }
-        
 
+        protected override byte[] GetData()
+        {
+            return File.ReadAllBytes(_jpgPath);
+        }
     }
 }
